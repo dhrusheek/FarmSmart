3285b9c8d44dd516efdc3a6049c992bc18471595
dhrusheek <itonbolu@gmail.com>
2026-01-31 16:14:01 +0530

    ci: deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/FarmSmart/",
+});
